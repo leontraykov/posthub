267d9e22431d0f46cf3f4bd86f26bae69a1327d3
Leonid <leon.traykov@gmail.com>
2024-05-05 13:30:49 -0400

    forced validation of records before saving

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,10 +35,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  Rails.logger = Logger.new($stdout)
+  config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+  config.log_level = :info
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,65 +2,77 @@
 
 # Script for populating the database with initial data for users, posts, and ratings.
 
+# Uncomment this method to be able to run validations before saving objects
+Rails.logger.info 'Validations OFF. If required uncomment validate_records method in seeds.rb
+and the lines where this method is applied'
+# def validate_records(records, model)
+#   Rails.logger.info "Validating #{model.name.downcase.pluralize} =>"
+#   valid_records = records.filter { |record| model.new(record).valid? }
+#   Rails.logger.info "#{model.name.pluralize} are valid, ready to save"
+#   valid_records
+# end
+
 # Creating users
-Rails.logger.debug 'Creating users =>'
+Rails.logger.info 'Creating users =>'
 users = 100.times.map { |i| { login: "user_#{i}", created_at: Time.current, updated_at: Time.current } }
-User.insert_all(users) # Bulk insert to optimize performance.
-Rails.logger.debug 'Users have been created!'
+# users = validate_records(users, User)
+User.insert_all(users)
+Rails.logger.info 'Users have been created!'
 
 # Creating IP addresses
-Rails.logger.debug 'Creating IP adresses =>'
-ips = 50.times.map { |_i| "192.168.#{rand(1..254)}.#{rand(1..254)}" } # Generate random IP addresses.
-Rails.logger.debug 'IP adresses have been created!'
+Rails.logger.info 'Creating IP adresses =>'
+ips = 50.times.map { |_i| "192.168.#{rand(1..254)}.#{rand(1..254)}" }
+Rails.logger.info 'IP adresses have been created!'
 
 # Creating posts
-Rails.logger.debug 'Creating posts =>'
+Rails.logger.info 'Creating posts =>'
 posts = []
-total_posts = 200_000 # Total number of posts to create.
+total_posts = 200_000
 total_posts.times do |i|
-  user_id = rand(1..100) # Randomly assign a user id from the created users.
-  ip = ips[i % ips.length] # Cycle through the IP list for each post.
-  posts << {
+  post = {
     title: Faker::Lorem.sentence(word_count: 3),
     body: Faker::Lorem.sentence(word_count: 5),
-    user_id:,
-    ip:,
+    user_id: rand(1..100),
+    ip: ips[i % ips.length],
     created_at: Time.current,
     updated_at: Time.current
   }
-  # Insert 10,000 posts at a time to optimize database insertion performance.
+  posts << post
   next unless posts.size >= 10_000
 
+  # posts = validate_records(posts, Post)
   Post.insert_all(posts)
+  Rails.logger.info "#{i + 1} / #{total_posts} posts created..."
   posts.clear
-  Rails.logger.debug "#{i + 1} / #{total_posts} posts created..."
 end
-Post.insert_all(posts) unless posts.empty? # Insert any remaining posts.
-Rails.logger.debug 'All posts have been created!'
+Post.insert_all(posts) unless posts.empty?
+Rails.logger.info 'All posts have been created!'
 
 # Creating ratings
-Rails.logger.debug 'Creating ratings =>'
+Rails.logger.info 'Creating ratings =>'
 ratings = []
 count_created_ratings = 0
 Post.limit(150_000).find_each do |post|
-  ratings << {
+  rating = {
     post_id: post.id,
-    user_id: rand(1..100), # Assign a random user to rate the post.
-    value: rand(1..5), # Random rating value between 1 and 5.
+    user_id: rand(1..100),
+    value: rand(1..5),
     created_at: Time.current,
     updated_at: Time.current
   }
-  # Insert 10,000 ratings at a time for performance.
+  ratings << rating
   if ratings.size >= 10_000
+    # ratings = validate_records(ratings, Rating)
     Rating.insert_all(ratings)
     count_created_ratings += ratings.size
-    Rails.logger.debug "Ratings created: #{count_created_ratings}"
+    Rails.logger.info "Ratings created: #{count_created_ratings}"
     ratings.clear
   end
 end
 if ratings.any?
+  # ratings = validate_records(ratings, Rating)
   Rating.insert_all(ratings)
   count_created_ratings += ratings.size
-  Rails.logger.debug "Ratings created: #{count_created_ratings}"
+  Rails.logger.info "Final ratings created: #{count_created_ratings}"
 end
-Rails.logger.debug 'All ratings have been created!'
+Rails.logger.info 'All ratings have been created!'
